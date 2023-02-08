8e6b8747d40d352653de9085694b9dc1ae180db5
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-02-08 11:21:28 +0100

    Fixed issue where the list of threads was expanded for every .post() method

--- src/Java/Task2/Worker.java
+++ src/Java/Task2/Worker.java
@@ -2,8 +2,6 @@ package Java.Task2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Worker extends Thread {
     List<Thread> threads;
@@ -13,20 +11,17 @@ public class Worker extends Thread {
      * @param numberOfThreads the amount of threads we want to execute tasks
      */
     public Worker(int numberOfThreads) {
-        threads = new ArrayList<>();
+        threads = new ArrayList<>(numberOfThreads);
         for(int i = 0; i < numberOfThreads; i++){
-            threads.add(null);
+            threads.add(new Thread());
         }
     }
 
     public int findFreeThread(Thread task){
         for(int i = 0; i < threads.size(); i++){
             // If we have a free slot, fill the slot with a new Thread
-            if (threads.get(i)==null) {
-                threads.add(i, task);
-                return i;
-            } else if (!threads.get(i).isAlive()){
-                threads.add(i, task);
+            if (!threads.get(i).isAlive()){
+                threads.set(i, task);
                 return i;
             }
         }
@@ -38,6 +33,10 @@ public class Worker extends Thread {
         //Busy Wait
         while((pos = findFreeThread(task)) == -1) { }
         threads.get(pos).start();
+    }
+
+    public int size(){
+        return threads.size();
     }
 
     public void joinWorkers() {
