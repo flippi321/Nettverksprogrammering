77d9bbe2afbabefc67a71b8e26d6da01399c4d2b
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-02-15 14:19:51 +0100

    Transfered code from Server to Thread, to support multiple Clients

--- src/Java/Task3/CalculatorThread.java
+++ src/Java/Task3/CalculatorThread.java
@@ -1,16 +1,55 @@
 package Java.Task3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class CalculatorThread implements Runnable {
+public class CalculatorThread extends Thread {
+    InputStreamReader readConnection;
+    BufferedReader reader;
+    PrintWriter writer;
     Socket socket;
 
-    public CalculatorThread (){
-
+    public CalculatorThread (Socket socket){
+        this.socket = socket;
     }
 
     @Override
     public void run() {
+        try {
+            // Communicates with Client
+            this.readConnection = new InputStreamReader(socket.getInputStream());
+            this.reader = new BufferedReader(readConnection);
+            this.writer = new PrintWriter(socket.getOutputStream(), true);
+
+            writer.println("Connected to Client. Type your message");
+
+            // Reader to catch information sent by Client
+            //Scanner commandReader = new Scanner(System.in);
+
+            // Listen for inputs
+            String line = reader.readLine();
+            while (!line.equals("")) {
+                writer.println(line);  // sender teksten til tjeneren
+                String response = reader.readLine();  // mottar respons fra tjeneren
+                System.out.println("Fra tjenerprogrammet: " + response);
+                line = reader.readLine();
+            }
+
+            reader.close();
+            writer.close();
+            readConnection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        List list = new ArrayList<>();
 
     }
 }
