6ded287fbe99e3e8b0da5f19bd7c5f35c14cee09
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-02-15 11:30:54 +0100

    Created new Class CalculatorThread to handle a single connection

---
+++ src/Java/Task3/CalculatorThread.java
@@ -0,0 +1,16 @@
+package Java.Task3;
+
+import java.net.Socket;
+
+public class CalculatorThread implements Runnable {
+    Socket socket;
+
+    public CalculatorThread (){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
