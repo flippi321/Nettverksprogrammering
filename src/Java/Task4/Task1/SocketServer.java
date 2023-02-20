4e93dd96c02248e85f77f9b65c9ed1acd515df01
flippi321 <90700334+flippi321@users.noreply.github.com>
2023-02-20 12:28:42 +0100

    Changed communication to UDP

--- src/Java/Task4/Task1/SocketServer.java
+++ src/Java/Task4/Task1/SocketServer.java
@@ -3,18 +3,26 @@ package Java.Task4.Task1;
 import Java.Task3.Task1.CalculatorThread;
 
 import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 class SocketServer {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
     final int PORTNR = 80;
+    AsynchronousSocketChannel channel = AsynchronousSocketChannel.open();
     ServerSocket server = new ServerSocket(PORTNR);
     System.out.println("Server Created. Waiting for Input...\n\n");
+
     while(true){
-      Socket connection = server.accept();  // Waiting for connection
-      CalculatorThread calculator = new CalculatorThread(connection);
-      calculator.start();
+      InetSocketAddress hostAddress = new InetSocketAddress("localhost", 2020);
+      Future<Void> future = channel.connect(hostAddress);
+
+      future.get();//wait for connection
     }
   }
 }
